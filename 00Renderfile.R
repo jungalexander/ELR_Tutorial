e812005b3237af1b055713333a41758e954105e1
jungalexander <jungalexander.email@gmail.com>
2020-11-14 21:13:43 +0100

    Intial commit

---
+++ 00Renderfile.R
@@ -0,0 +1,5 @@
+#setwd("~/Documents/1 UNIVERSITÄT/01_Master/HIWI RWTH/lavaanGUI Tutorial ")
+#setwd("C:/Users/neles/sciebo/Tessa/lavaanGUI Tutorial")
+bookdown::render_book("00_Index.Rmd", "bookdown::gitbook")
+
+
